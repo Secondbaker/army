50f9e18802d90826d1c87d09f0056b4073de0207
secondbaker <secondbaker@gmail.com>
2020-04-16 10:20:23 -0400

    There are three routes needed to make auth0 work. 1.  A route to the authentication process, in this case: get 'auth/auth0', as: 'authentication' 2.  A callback from successful authentication, in this case: get 'auth/auth0/callback' => 'auth0#callback' 3.  A route for failed authentication, in this case: get 'auth/failute' => 'auth0#failure'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  get 'home/dashboard'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+
+  get 'dashboard' => 'home#dashboard'
+
+  get 'auth/auth0', as: 'authentication'        # Triggers authentication process
+  get 'auth/auth0/callback' => 'auth0#callback' # Authentication successful
+  get 'auth/failure' => 'auth0#failure'         # Authentication fail
 end
